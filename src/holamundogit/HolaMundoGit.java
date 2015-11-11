19e6471b5301de6abca595132c9877ab583bb67e
a044888329b <a044888329b@s09-pc15>
2015-11-11 20:03:30 +0100

    Inicio

---
+++ src/holamundogit/HolaMundoGit.java
@@ -0,0 +1,21 @@
+
+
+package holamundogit;
+
+/**
+ *
+ * @author a044888329b
+ */
+public class HolaMundoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       System.out.println("Hola mundo");
+       System.out.println("Rojo");
+        
+    }
+    
+}
